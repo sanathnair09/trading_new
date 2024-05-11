0b7c4596216a5587206eb5ad2951ed7d267fb8f2
Sanath Nair <sanathnair09@gmail.com>
2024-05-11 11:11:37 -0700

    Added abstract methods

--- src/brokers/broker_abc.py
+++ src/brokers/broker_abc.py
@@ -1,15 +1,36 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 
+from utils.data_types import OptionOrder, StockOrder
+
 
 class Broker(ABC):
     def __init__(self, equity_report_file: Path, option_report_file: Path) -> None:
         self._equity_report_file = equity_report_file
         self._option_report_file = option_report_file
-    
+
     @abstractmethod
     def name(self) -> str:
         raise NotImplementedError
-    
+
+    @abstractmethod
+    def login(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def buy(self, order: StockOrder) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def sell(self, order: StockOrder) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def buy_option(self, order: OptionOrder) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def sell_option(self, order: OptionOrder) -> None:
+        raise NotImplementedError
 
     
