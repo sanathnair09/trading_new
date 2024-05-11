467b57ac6aa407ed97ffd2362bce2f717fb0b536
Sanath Nair <sanathnair09@gmail.com>
2024-05-11 11:14:07 -0700

    Added abstract methods for specific orders

--- src/brokers/broker_abc.py
+++ src/brokers/broker_abc.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from utils.data_types import OptionOrder, StockOrder
 
+OrderResult = str | dict | None
+
 
 class Broker(ABC):
     def __init__(self, equity_report_file: Path, option_report_file: Path) -> None:
@@ -33,4 +35,34 @@ class Broker(ABC):
     def sell_option(self, order: OptionOrder) -> None:
         raise NotImplementedError
 
-    
+    @abstractmethod
+    def _market_buy(self, order: StockOrder) -> OrderResult:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _market_sell(self, order: StockOrder) -> OrderResult:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _limit_buy(self, order: StockOrder) -> OrderResult:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _limit_sell(self, order: StockOrder) -> OrderResult:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _buy_call_option(self, order: OptionOrder) -> OrderResult:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _sell_call_option(self, order: OptionOrder) -> OrderResult:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _buy_put_option(self, order: OptionOrder) -> OrderResult:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _sell_put_option(self, order: OptionOrder) -> OrderResult:
+        raise NotImplementedError
